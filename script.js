411e202b1c5d8d518cef02b3032e438f10d0c084
nashEm8 <emanuel82016@outlook.com>
2023-07-05 19:58:59 -0300

    Arquivo js

---
+++ script.js
@@ -0,0 +1,18 @@
+let button_projetos = document.querySelector('.projetos');
+let button_habilidades = document.querySelector('.habilidades');
+let show_projetos = document.querySelector('.show-projetos');
+let show_habilidades = document.querySelector('.show-habilidades');
+
+button_projetos.addEventListener('click', ()=>{
+  button_projetos.classList.toggle('ativo');
+  button_habilidades.classList.remove('ativo');
+  show_habilidades.style.display = 'none';
+  show_projetos.style.display = 'flex';
+});
+
+button_habilidades.addEventListener('click', ()=>{
+  button_habilidades.classList.toggle('ativo');
+  button_projetos.classList.remove('ativo');
+  show_projetos.style.display = 'none';
+  show_habilidades.style.display = 'block';
+});
